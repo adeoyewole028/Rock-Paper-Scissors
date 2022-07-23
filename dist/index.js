b4cff1432d7a17ef90e0bbea7dda7debb77c97ff
adeoyewole028 <adeoyewole028@gmail.com>
2022-07-23 21:46:07 +0100

    added new UI,

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,17 @@
 const btn = Array.from(document.querySelectorAll("button"));
 const rockEl = document.querySelector("#score");
 const replayEl = document.querySelector("#play-again");
+const message = document.querySelector("#message");
 
 // To store computer and player scores
 playerScore = 0;
 computerScore = 0;
-let value = "";
+let computerChoice = "";
+let playerChoice = "";
 
+// To play again
 let playAgain = document.createElement("button");
-playAgain.setAttribute("class", "border p-5");
+playAgain.setAttribute("class", "border p-5 bg-white text-white");
 playAgain.innerText = "Play Again?";
 playAgain.addEventListener("click", () => {
   location.reload();
@@ -16,55 +19,78 @@ playAgain.addEventListener("click", () => {
 
 // computer random choices
 function computerPlay() {
-  let computerChoice = ["rock", "paper", "scissors"];
+  let computerChoices = ["rock", "paper", "scissors"];
   let randomChoice =
-    computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
   return randomChoice;
 }
 
+// player and computer choices
 function play(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    value = `Tie! Computer played: ${computerSelection}, Play again`;
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      value = `Computer plays: ${computerSelection}`;
+    message.innerHTML = `It's a tie! ${computerSelection} ties with ${playerSelection}`;
+    computerChoice = `${computerSelection}`;
+    playerChoice = ` ${playerSelection}`;
+    console.log(playerSelection);
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Paper") {
+      message.innerHTML = `Computer ${computerSelection} beats your ${playerSelection}🤦‍♂️`;
+      computerChoice = `${computerSelection}`;
+      playerChoice = ` ${playerSelection}`;
       computerScore++;
     } else {
-      value = `Computer plays: ${computerSelection}`;
+      message.innerHTML = `Your ${playerSelection} smashes Computer's ${computerSelection} 😁`;
+      computerChoice = `${computerSelection}`;
       playerScore++;
     }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "scissors") {
-      value = `Computer plays: ${computerSelection}`;
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Scissors") {
+      message.innerHTML = `Computer ${computerSelection} cuts your ${playerSelection}🤦‍♂️`;
+      computerChoice = `${computerSelection}`;
+      playerChoice = ` ${playerSelection}`;
       computerScore++;
     } else {
-      value = `Computer plays: ${computerSelection}`;
+      message.innerHTML = `Your ${playerSelection} beats ${computerSelection} 😁`;
+      computerChoice = `${computerSelection}`;
+      playerChoice = ` ${playerSelection}`;
       playerScore++;
     }
-  } else if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      value = `Computer plays: ${computerSelection}`;
+  } else if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock") {
+      message.innerHTML = `${computerSelection} smashes ${playerSelection}🤦‍♂️`;
+      computerChoice = `${computerSelection}`;
+      playerChoice = ` ${playerSelection}`;
       computerScore++;
     } else {
-      value = `Computer plays: ${computerSelection}`;
+      message.innerHTML = `Your ${playerSelection} cuts ${computerSelection} 😁`;
+      computerChoice = `${computerSelection}`;
+      playerChoice = ` ${playerSelection}`;
       playerScore++;
     }
   }
-  rockEl.innerHTML = `${value} <br> Player: ${playerScore} point <br> Computer: ${computerScore} point`;
-  console.log(`player score ${playerScore} computer score ${computerScore}`);
-  scores();
+
+  rockEl.innerHTML = ` <div class="flex flex-col text-center space-y-6">
+  <div id="player-choice">${playerChoice}</div>
+  <div id="player-score">Player: ${playerScore}</div>
+</div>
+<div class="flex flex-col text-center space-y-6">
+  <div id="computer-choice">${computerChoice}</div>
+  <div id="computer-score">computer: ${computerScore}</div>
+</div>`;
+
+  getScores();
 }
 
 // To report a winner or loser at the end of game
-function scores(rounds = 5) {
+function getScores(rounds = 5) {
   if (playerScore === rounds) {
     console.log("player champ");
-    rockEl.innerHTML = `${value} <br> You Won!: ${playerScore} Points`;
+    rockEl.innerHTML = `You Won! ${playerScore} Points 😎`;
     btn.forEach((button) => button.setAttribute("disabled", ""));
     replayEl.appendChild(playAgain);
   } else if (computerScore === rounds) {
     console.log("comp champ");
-    rockEl.innerHTML = `${value} <br> Computer Won!: ${computerScore} Points`;
+    rockEl.innerHTML = `Computer Won! ${computerScore} Points ☹`;
     btn.forEach((button) => button.setAttribute("disabled", ""));
     replayEl.appendChild(playAgain);
   }
@@ -74,7 +100,11 @@ function scores(rounds = 5) {
 function startGame(user, comp) {
   btn.map((button) =>
     button.addEventListener("click", (e) => {
-      play((user = e.target.value), (comp = computerPlay()));
+      let player = e.target.value;
+      user = player.slice(0, 1).toUpperCase() + player.slice(1);
+      let computer = computerPlay();
+      comp = computer.slice(0, 1).toUpperCase() + computer.slice(1);
+      play(user, comp);
 
       console.log(`user: ${user}`);
       console.log(`computer: ${comp}`);
